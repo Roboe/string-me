7881047d0998a51bcd0abc3b29dc1b6e615ca4ec
Roboe <robermarfer@gmail.com>
2014-03-22 22:57:51 +0100

    DFactoriales y EProblemasRico

--- src/programame2014/EProblemasRico.java
+++ src/programame2014/EProblemasRico.java
@@ -1,9 +1,29 @@
 package programame2014;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Roboe
+ */
 public class EProblemasRico {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int casos = Integer.parseInt(br.readLine());
+		for (int i = 0; i < casos; i++) {
+			String[] input = br.readLine().split(" ");
+			int comps = Integer.parseInt(input[0]);
+			int cap = Integer.parseInt(input[1]);
+			int dif = Integer.parseInt(input[2]);
+			long suma = cap;
+			while (comps > 1) {
+				cap -= dif;
+				suma += cap;
+				comps--;
+			}
+			System.out.println(suma);
+		}
 	}
-
 }
