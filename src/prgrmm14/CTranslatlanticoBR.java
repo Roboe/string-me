ce8db4727a312f3c4579cd756a1c4177a3c4d64b
Roboe <robermarfer@gmail.com>
2014-03-23 16:47:43 +0100

    C Transatlántico con BufferedReader

---
+++ src/prgrmm14/CTranslatlanticoBR.java
@@ -0,0 +1,46 @@
+package prgrmm14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author Roboe
+ */
+public class CTranslatlanticoBR {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int pLength;
+		while ((pLength = Integer.parseInt(br.readLine())) != 0) {
+			int[] pasajeros = new int[pLength];
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < pLength; i++) {
+				pasajeros[i] = Integer.parseInt(st.nextToken());
+			}
+			int casos = Integer.parseInt(br.readLine());
+			while (casos > 0) {
+				String[] input = br.readLine().split(" ");
+				if (input[0].equals("CONSULTA")) {
+					System.out
+							.println(pasajeros[Integer.parseInt(input[1]) - 1]);
+				} else if (input[0].equals("EMBARQUE")) {
+					int embarcar = Integer.parseInt(input[1]);
+					int i = 0, j = 0;
+					while (i < pLength) {
+						if (pasajeros[i] != embarcar) {
+							pasajeros[j] = pasajeros[i];
+							j++;
+						}
+						i++;
+					}
+					pLength = j;
+					System.out.println(pLength);
+				}
+				casos--;
+			}
+			System.out.println("*");
+		}
+	}
+}
