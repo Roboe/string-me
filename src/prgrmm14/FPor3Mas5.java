2584c779b18f9fb8a7a6cf4a2f012f3b389b2326
Roboe <robermarfer@gmail.com>
2014-03-23 23:52:56 +0100

    F Por 3 o más 5

--- src/prgrmm14/FPor3Mas5.java
+++ src/prgrmm14/FPor3Mas5.java
@@ -8,28 +8,46 @@ import java.io.InputStreamReader;
  * @author Roboe
  */
 public class FPor3Mas5 {
-	/*
-	 * TODO Sigue tardando bastante con números muy grandes terminados en 0, 2,
-	 * 5 y 7
-	 */
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input;
 		while (!(input = br.readLine()).equals("0")) {
-			long numero = Long.parseLong(input);
+			int numero = Integer.parseInt(input);
 			System.out.println((isPossible(numero)) ? "SI" : "NO");
 		}
 	}
 
-	private static boolean isPossible(long n) {
-		long c = n % 10;
-		if (c == 1 || c == 3 || c == 6 || c == 8 || c == 9)
-			return true;
-		else if (n - 5 > 0 && isPossible(n - 5))
-			return true;
-		else if (n % 3 == 0 && isPossible(n / 3))
-			return true;
-		else
-			return false;
+	private static boolean isPossible(int n) {
+		if (resultados[n] == null) {
+			long c = n % 10;
+			if (c == 1 || c == 3 || c == 6 || c == 8 || c == 9
+					|| (c == 4 && n > 5))
+				resultados[n] = Estado.POSIBLE;
+			else if (n % 3 == 0 && isPossible(n / 3))
+				resultados[n] = Estado.POSIBLE;
+			else if (n - 5 > 0 && isPossible(n - 5))
+				resultados[n] = Estado.POSIBLE;
+			else
+				resultados[n] = Estado.NO_POSIBLE;
+		}
+		return resultados[n].toBoolean();
 	}
+
+	private static Estado[] resultados = new Estado[20001];
+}
+
+enum Estado {
+	NO_POSIBLE {
+		public boolean toBoolean() {
+			return false;
+		}
+	},
+	POSIBLE {
+		public boolean toBoolean() {
+			return true;
+		}
+	};
+
+	abstract boolean toBoolean();
+
 }
