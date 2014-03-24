43a7f3663afb0e862bcba83ae360380e8fdf44a9
Roboe <robermarfer@gmail.com>
2014-03-24 23:56:13 +0100

    Probando ideas locas en el Hola mundo

--- src/acptalrto/N116HolaMundoXDXDXD.java
+++ src/acptalrto/N116HolaMundoXDXDXD.java
@@ -1,22 +1,15 @@
 package acptalrto;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-
-import util.Entrada;
 
 /**
  * @author Roboe
  * @link https://www.aceptaelreto.com/problem/statement.php?id=116
- * @result AC
+ * @result AC p2 t0.005 m68
  */
 public class N116HolaMundoXDXDXD {
-
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				Entrada.getEntrada(Entrada.ACPTALRTO, "N116")));
-		int casos = Integer.parseInt(br.readLine());
+		int casos = System.in.read() - '0';
 		while (casos > 0) {
 			System.out.println("Hola mundo.");
 			casos--;
