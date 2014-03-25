41dd7da8fa6845942f1fd14bc8a31b3b650499e5
Roboe <robermarfer@gmail.com>
2014-03-25 21:41:53 +0100

    I Saltos de trampolín

--- src/prgrmm14/ISaltosTrampolin.java
+++ src/prgrmm14/ISaltosTrampolin.java
@@ -1,9 +1,27 @@
 package prgrmm14;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import util.Entrada;
+
+/**
+ * @author Roboe
+ */
 public class ISaltosTrampolin {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				Entrada.getEntrada(Entrada.PRGRMM14, "I")));
+		String input;
+		while ((input = br.readLine()) != null) {
+			String[] puntuaciones = input.split(" ");
+			Arrays.sort(puntuaciones);
+			System.out.printf("%.0f\n", (Double.parseDouble(puntuaciones[2])
+					+ Double.parseDouble(puntuaciones[3]) + Double
+					.parseDouble(puntuaciones[4])) * 2);
+		}
 	}
-
 }
