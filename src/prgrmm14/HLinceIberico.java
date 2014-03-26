e66ef7c461288b8d508aa7f83fbf1c1f0186df73
Roboe <robermarfer@gmail.com>
2014-03-26 22:05:26 +0100

    H Lince ibérico

--- src/prgrmm14/HLinceIberico.java
+++ src/prgrmm14/HLinceIberico.java
@@ -1,9 +1,32 @@
 package prgrmm14;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import util.Entrada;
+
+/**
+ * @author Roboe
+ */
 public class HLinceIberico {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				Entrada.getEntrada(Entrada.PRGRMM14, "H")));
+		int casos = Integer.parseInt(br.readLine());
+		for (int i = 0; i < casos; i++) {
+			char[] input = br.readLine().toCharArray();
+			int vallas = 0;
+			for (int j = 0; j < input.length; j++) {
+				// System.out.print(j + " ");
+				if (input[j] == '.') {
+					vallas++;
+					j += 2;
+				}
+			}
+			System.out.println(vallas);
+		}
 	}
 
 }
