271c40e4e2a1dc0da7aff06032727e69f7f67034
Roboe <robermarfer@gmail.com>
2014-04-02 19:13:40 +0200

    H enviado al juez. AC

--- src/prgrmm14/HLinceIberico.java
+++ src/prgrmm14/HLinceIberico.java
@@ -4,22 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import util.Entrada;
-
 /**
  * @author Roboe
+ * @see https://www.aceptaelreto.com/problem/statement.php?id=194
+ * @result AC p1 t1.419 m3585
  */
 public class HLinceIberico {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				Entrada.getEntrada(Entrada.PRGRMM14, "H")));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int casos = Integer.parseInt(br.readLine());
 		for (int i = 0; i < casos; i++) {
 			char[] input = br.readLine().toCharArray();
 			int vallas = 0;
 			for (int j = 0; j < input.length; j++) {
-				// System.out.print(j + " ");
 				if (input[j] == '.') {
 					vallas++;
 					j += 2;
