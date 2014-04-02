17d5844916a113e7d6c15f819e7adda276f700a6
Roboe <robermarfer@gmail.com>
2014-04-02 19:18:40 +0200

    J enviado al juez. AC

--- src/prgrmm14/JSudokus.java
+++ src/prgrmm14/JSudokus.java
@@ -4,16 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import util.Entrada;
-
 /**
  * @author Roboe
+ * @see https://www.aceptaelreto.com/problem/statement.php?id=196
+ * @result AC p1 t1.069 m2048
  */
 public class JSudokus {
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				Entrada.getEntrada(Entrada.PRGRMM14, "J")));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int casos = Integer.parseInt(br.readLine());
 		for (int i = 0; i < casos; i++) {
 			String input = br.readLine();
