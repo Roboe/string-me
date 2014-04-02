03a3512bc3f8e889d141650039681e607280edbb
Roboe <robermarfer@gmail.com>
2014-04-02 19:27:43 +0200

    Añadido enlace al juez

--- src/prgrmm14/ASolitario.java
+++ src/prgrmm14/ASolitario.java
@@ -1,5 +1,9 @@
 package prgrmm14;
 
+/**
+ * @author
+ * @see https://www.aceptaelreto.com/problem/statement.php?id=187
+ */
 public class ASolitario {
 
 	public static void main(String[] args) {
--- src/prgrmm14/GNumsReversibles.java
+++ src/prgrmm14/GNumsReversibles.java
@@ -1,5 +1,9 @@
 package prgrmm14;
 
+/**
+ * @author
+ * @see https://www.aceptaelreto.com/problem/statement.php?id=193
+ */
 public class GNumsReversibles {
 
 	public static void main(String[] args) {
